d6860b2a251f24cefd6a146a160c13c15da482e8
Abbas Hussain Muzammil <a.lnu25@vikes.csuohio.edu>
2023-09-08 01:47:02 -0400

    added app gif completed video

--- app/src/main/java/com/example/usingintent/MainActivity.java
+++ app/src/main/java/com/example/usingintent/MainActivity.java
@@ -3,8 +3,10 @@ package com.example.usingintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -12,6 +14,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Initialize SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("MyPrefs", MODE_PRIVATE);
+        int launchCount = sharedPreferences.getInt("launchCount", 0);
+
+        // Increment the count
+        launchCount++;
+
+        // Store the updated count
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putInt("launchCount", launchCount);
+        editor.apply();
+
+        // Display the count
+        TextView countTextView = findViewById(R.id.textView2); // Replace with your TextView ID
+        countTextView.setText("Count: " + launchCount);
+
+
     }
 
     public void onClick(View view) {
